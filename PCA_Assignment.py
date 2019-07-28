335bb2cb6b45bc82171db0370c25939965d21ba3
chandanawasthi91 <40992637+chandanawasthi91@users.noreply.github.com>
2019-07-28 19:09:04 +0530

    Add files via upload

--- PCA_Assignment.py
+++ PCA_Assignment.py
@@ -3,6 +3,12 @@ import numpy as np
 wine = pd.read_csv("C:\\Users\\cawasthi\\Desktop\\Data Science\\R ML Code\\dimensionality reduction\\wine.csv")
 wine.describe()
 wine.head()
+'''
+Perform Principal component analysis and perform clustering using first 
+3 principal component scores (both heirarchial and k mean clustering(scree plot or elbow curve) and obtain 
+optimum number of clusters and check whether we have obtained same number of clusters with the original data 
+(class column we have ignored at the begining who shows it has 3 clusters)df
+'''
 
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
